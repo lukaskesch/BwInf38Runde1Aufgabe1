3b23ebba43d32702c611d99e1cd7ffb8bb1f4912
Lukas Kesch <Lukas.Kesch@hotmail.de>
2019-09-27 20:03:35 +0200

    Start

--- BwInfAufgabe1/MainWindow.xaml.cs
+++ BwInfAufgabe1/MainWindow.xaml.cs
@@ -24,5 +24,82 @@ namespace BwInfAufgabe1
         {
             InitializeComponent();
         }
+
+        private void ButtonCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            string[] EingabeArray;
+            int[] Farbnummern;
+            int[] Farbpaare;
+            string EingabeString;
+            int[,] Blumenbeet = CreateBlumenbeet();
+
+            //Konvertiere die Eingabe in das EingabeArray
+            EingabeString = TextBoxInput.Text;
+            EingabeArray = EingabeString.Split('\n');
+            try
+            {
+                EingabeString = TextBoxInput.Text;
+                EingabeArray = EingabeString.Split('\n');
+
+                Farbpaare = GetFarbpaare(EingabeArray);
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        
+
+        private int[,] CreateBlumenbeet()
+        {
+            //Array für das Blumenbeet
+            //Erste Spalte speichert den Nummerncode der Blumenfarbe und der Index gibt an welchem Steckplatz 
+            //Zweite Spalte ist wie viele Nebenblumen ein Steckplatz hat
+            int[,] Blumenbeet = new int[9,2];
+
+            //Belegt jeden Index der Blumennummer spalte mit einer Prüfziffer
+            for(int i = 0; i<Blumenbeet.Length; i++)
+            {
+                Blumenbeet[i, 0] = -1;
+            }
+
+            //Definiert wie viele Nebenblumen ein Steckplatz hat
+            Blumenbeet[1, 0] = 2;
+            Blumenbeet[1, 1] = 4;
+            Blumenbeet[1, 2] = 4;
+            Blumenbeet[1, 3] = 3;
+            Blumenbeet[1, 4] = 6;
+            Blumenbeet[1, 5] = 3;
+            Blumenbeet[1, 6] = 4;
+            Blumenbeet[1, 7] = 4; 
+            Blumenbeet[1, 8] = 2; 
+
+
+            return Blumenbeet;
+        }
+
+        private int BlumenNameToBlumenNummer(string Blumenname)
+        {
+            switch (Blumenname)
+            {
+                case "blau":
+                    return 1;
+                case "gelb":
+                    return 2;
+                case "gruen":
+                    return 3;
+                case "orange":
+                    return 4;
+                case "rosa":
+                    return 5;
+                case "rot":
+                    return 6;
+                case "tuerkis":
+                    return 7;
+                default:
+                    return -1;
+            }
+        }
     }
 }
